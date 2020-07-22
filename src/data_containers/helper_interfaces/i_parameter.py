7fa689695ad3716de320f362081b09d74bcc7daf
SeanvdMeer <18538762+minisean@users.noreply.github.com>
2020-07-22 15:02:20 +0200

    Added ability to update IParameter with optimized trial result parameters

--- src/data_containers/helper_interfaces/i_parameter.py
+++ src/data_containers/helper_interfaces/i_parameter.py
@@ -1,5 +1,6 @@
 from typing import (Dict)
 import cirq
+from openfermioncirq.optimization import OptimizationTrialResult
 
 
 class IParameter:
@@ -15,6 +16,12 @@ class IParameter:
     def __init__(self, parameters: Dict[str, float]):
         self._p = parameters
 
+    def update(self, r: OptimizationTrialResult):
+        if len(r.optimal_parameters) != len(self.dict):
+            raise IndexError("Dimensions of optimized trial result does not correspond to IParameter")
+        for i, key in enumerate(self.dict.keys()):
+            self[key] = r.optimal_parameters[i]
+
     def __iter__(self):
         return self.dict.__iter__()
 
@@ -23,6 +30,9 @@ class IParameter:
 
     def __getitem__(self, item: str):
         return self.dict.__getitem__(item)
+
+    def __setitem__(self, key: str, value: float):
+        return self.dict.__setitem__(key, value)
 
     def __add__(self, other):
         return IParameter({**self.dict, **other.dict})
--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ import cirq
 from src.data_containers.model_hydrogen import HydrogenAnsatz
 from src.processors.processor_quantum import QPU
 from src.processors.processor_classic import CPU
-from src.data_containers.helper_interfaces.i_hamiltonian import IHamiltonian
 
 
 if __name__ == '__main__':
@@ -14,8 +13,12 @@ if __name__ == '__main__':
     circuit = ansatz.circuit
     circuit.append(cirq.measure(*ansatz.qubits, key='x'))  # Temporary forced measurement on x axis
     resolved_circuit = QPU.get_resolved_circuit(circuit, parameters)
-    print(circuit)
+    print(resolved_circuit)
 
     # Get variational study
     result = CPU.get_optimized_state(w=ansatz, max_iter=10)
-    print(f'Operator expectation value: {result.optimal_value}, with parameters: {result.optimal_parameters}')
+    print(f'Operator expectation value: {result.optimal_value}\nOperator parameters: {result.optimal_parameters}')
+
+    parameters.update(r=result)
+    resolved_circuit = QPU.get_resolved_circuit(circuit, parameters)
+    print(resolved_circuit)
