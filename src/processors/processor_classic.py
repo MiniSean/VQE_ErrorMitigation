659b5d3114a1fe13e658b5e678af18a3abf859db
SeanvdMeer <18538762+minisean@users.noreply.github.com>
2020-08-25 19:00:18 +0200

    Added a potential way to optimize molecular data

--- src/processors/processor_classic.py
+++ src/processors/processor_classic.py
@@ -7,13 +7,32 @@ from openfermioncirq.optimization import OptimizationTrialResult
 from src.processors.processor_quantum import QPU
 from src.data_containers.helper_interfaces.i_hamiltonian import IHamiltonian
 from src.data_containers.helper_interfaces.i_wave_function import IWaveFunction
+from src.data_containers.helper_interfaces.i_parameter import IParameter
 
 
 class CPU:
 
     @staticmethod
-    def get_optimized_ground_state():
-        pass
+    def get_optimized_ground_state(w: IWaveFunction, qpu_iter: int, cpu_iter: int):
+        molecule_params = w.molecule_parameters
+        optimization_trail = list()
+
+        def optimize_func(x: np.ndarray) -> float:
+            for i, key in enumerate(molecule_params):
+                molecule_params.dict[key] = x[i]
+            w.update_molecule(molecule_params)
+            print(molecule_params)
+
+            trial_result = CPU.get_optimized_state(w=w, max_iter=qpu_iter)
+
+            optimization_trail.append(trial_result)
+            print(trial_result.optimal_value)
+            return trial_result.optimal_value
+
+        initial_values = np.fromiter(molecule_params.dict.values(), dtype=float)
+        method = 'Nelder-Mead'
+        options = {'maxiter': cpu_iter}
+        return optimize.minimize(fun=optimize_func, x0=initial_values, method=method, options=options)
 
     @staticmethod
     def get_optimized_state(w: IWaveFunction, max_iter: int) -> OptimizationTrialResult:
