a31e2e20bd5a67c6aa9954389c20dea1a58ed7b8
Eleanor Scerri <scerri@lorentz.leidenuniv.nl>
2020-09-07 09:20:23 +0200

    Generates relevant molecular data in subfolder

--- src/data_containers/model_hydrogen.py
+++ src/data_containers/model_hydrogen.py
@@ -1,7 +1,9 @@
 from typing import Sequence
 import cirq
 import numpy as np
+import os
 from openfermion import MolecularData
+from openfermionpsi4 import run_psi4
 
 from src.data_containers.helper_interfaces.i_wave_function import IGeneralizedUCCSD
 from src.data_containers.helper_interfaces.i_parameter import IParameter
@@ -14,16 +16,54 @@ class HydrogenAnsatz(IGeneralizedUCCSD):
         molecule = IParameter({'r0': .7414})  # .7414, 'r1': 1.
         super().__init__(molecule)
 
-    # IWaveFunction
     def _generate_molecule(self, p: IParameter) -> MolecularData:
         """Produce molecule that can be used by the hamiltonian.
         Using a singlet state with S = 0 to specify we are looking for the lowest singlet energy state.
         multiplicity = 2S + 1
         """
         geometry = [('H', (0., 0., 0.)), ('H', (0., 0., p['r0']))]  # , ('H', (0., p['r1'], 0.))
-        molecule = MolecularData(geometry=geometry, basis='sto-3g', multiplicity=1, charge=0, description=format(p['r0']))
-        molecule.load()
-        print(f'Reference (Full Configuration Energy: {molecule.fci_energy})')
+        basis = 'sto-3g'
+        multiplicity = 1
+        charge = 0
+        description = str(p['r0'])
+
+        # Change to whatever directory you want
+        cwd = os.getcwd()
+        data_directory = cwd+'/mol_data'
+
+        if not os.path.exists(data_directory):
+            os.mkdir(data_directory)
+
+        filename = data_directory+'/H2_'+description
+
+        run_scf = 1
+        run_mp2 = 1
+        run_cisd = 1
+        run_ccsd = 1
+        run_fci = 1
+        delete_input = False
+        delete_output = False
+        verbose = False
+
+        molecule = MolecularData(
+            geometry,
+            basis,
+            multiplicity,
+            description=description,
+            filename=filename)
+
+        if os.path.exists('{}.hdf5'.format(filename)):
+            molecule.load()
+
+        else:
+            molecule = run_psi4(molecule,
+                                verbose=verbose,
+                                run_scf=run_scf,
+                                run_mp2=run_mp2,
+                                run_cisd=run_cisd,
+                                run_ccsd=run_ccsd,
+                                run_fci=run_fci)
+
         return molecule
 
     # IWaveFunction
