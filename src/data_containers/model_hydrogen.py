a09f1f76fd247c27299c48040089651c2fa8775f
SeanvdMeer <18538762+minisean@users.noreply.github.com>
2020-09-09 16:22:33 +0200

    Combined functionality for generating molecular data

--- src/data_containers/model_hydrogen.py
+++ src/data_containers/model_hydrogen.py
@@ -21,29 +21,7 @@ class HydrogenAnsatz(IGeneralizedUCCSD):
         Using a singlet state with S = 0 to specify we are looking for the lowest singlet energy state.
         multiplicity = 2S + 1
         """
-        geometry = [('H', (0., 0., 0.)), ('H', (0., 0., p['r0']))]  # , ('H', (0., p['r1'], 0.))
-        molecule = MolecularData(geometry=geometry, basis='sto-3g', multiplicity=1, charge=0, description=format(p['r0']))
-        # Make sure parameter 'shell=true' is specified in the subprocess.Popen() call in _run_psi4.py
-        # (process = subprocess.Popen(['psi4', input_file, output_file], shell=True))
-        # Source 1: https://stackoverflow.com/questions/39269675/python-trouble-with-popen-filenotfounderror-winerror-2
-        # Source 2: https://stackoverflow.com/questions/42572582/winerror-2-the-system-cannot-find-the-file-specified-python
-        # Run (bash): cmake -H. -Bbuilddir -D CMAKE_INSTALL_PREFIX=D:\sean\programs\PyCharmProjects -G "Visual Studio 15 2017 Win64"
-        # Run (conda): cmake -B D:\sean\programs\PyCharmProjects\psi4 -D CMAKE_INSTALL_PREFIX=D:\sean\programs\PyCharmProjects -G "Visual Studio 15 2017 Win64"
-        # -DBLAS_TYPE=SYSTEM_NATIVE -DLAPACK_TYPE=SYSTEM_NATIVE
-        # (specify 64 bit VS compiler)
-        # Build from binary perhaps? http://www.psicode.org/psi4manual/master/conda.html
-        # molecule = run_psi4(molecule,
-        #                     run_scf=True,
-        #                     run_mp2=True,
-        #                     run_cisd=True,
-        #                     run_ccsd=True,
-        #                     run_fci=True,
-        #                     verbose=False,
-        #                     tolerate_error=True,
-        #                     memory=4000)
-        molecule.save()
-        molecule.load()
-        print(f'Reference (Full Configuration Energy: {molecule.fci_energy})')
+        geometry = [('H', (0., 0., 0.)), ('H', (0., 0., p['r0']))]
         basis = 'sto-3g'
         multiplicity = 1
         charge = 0
@@ -85,7 +63,7 @@ class HydrogenAnsatz(IGeneralizedUCCSD):
                                 run_cisd=run_cisd,
                                 run_ccsd=run_ccsd,
                                 run_fci=run_fci)
-
+        print(f'Reference (Full Configuration Energy: {molecule.fci_energy})')
         return molecule
 
     # IWaveFunction
