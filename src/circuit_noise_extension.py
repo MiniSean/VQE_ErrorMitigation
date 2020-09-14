27fe5195d9177ea61f93871a9aa2bb71b461db77
SeanvdMeer <18538762+minisean@users.noreply.github.com>
2020-09-14 18:25:31 +0200

    Refactored noise introduction to work with specified noise model

--- src/circuit_noise_extension.py
+++ src/circuit_noise_extension.py
@@ -2,43 +2,21 @@ import cirq
 import numpy as np
 import openfermion as of
 from random import uniform
-from typing import List
+from typing import List, Sequence, Callable
+
+from src.data_containers.helper_interfaces.i_parameter import IParameter
+from src.data_containers.helper_interfaces.i_wave_function import IWaveFunction
 
 
 # Allows the modification of QubitOperator lists by introducing arbitrary or white noise-channels
 class Noisify:
 
     @staticmethod
-    def introduce_noise(circuit: cirq.Circuit) -> cirq.Circuit:
-        """
-        Introduces noise operations after each gate operation in a circuit.
-        Current noise operations are based on a random rotation to anywhere on the Bloch sphere.
-        :param circuit: Collection of gate operation to introduce noise to
-        :return: Same circuit with noise operations included
-        """
-        # result = []
-        # for moment in circuit.moments:
-        #     for gate_op in moment.operations:
-        #         noise_list = Noisify.uniform_rotation_gate(1000)  # Create random noise operation per gate operation
-        #         new_gate_list = [gate_op]
-        #         for noise_op in noise_list:
-        #             noise_model = cirq.ConstantQubitNoiseModel(noise_op)
-        #             # Handle obscure noisy_operation function output
-        #             new_moment_list = noise_model.noisy_operation(operation=new_gate_list[0])
-        #             new_gate_list.append(new_moment_list[1])  # Collects the added noise moment after gate operation
-        #         # Append to new moment
-        #         result.append(new_gate_list)
-        # result = [val for sublist in result for val in sublist]  # Flatten list of list of cirq.Moments
-        result = Noisify.introduce_noise_tree(circuit.moments)
-        # Create new circuit and set order strategy
-        return cirq.Circuit(result, strategy=cirq.InsertStrategy.EARLIEST)
-
-    @staticmethod
-    def introduce_noise_tree(op_tree: cirq.OP_TREE) -> cirq.OP_TREE:
+    def introduce_noise_type(op_tree: cirq.OP_TREE, noise_func: Callable[[], List[cirq.Gate]]) -> cirq.OP_TREE:
         result = []
         for op_list in op_tree:
             for gate_op in op_list:
-                noise_list = Noisify.uniform_rotation_gate(1000)  # Create random noise operation per gate operation
+                noise_list = noise_func()  # Create random noise operation per gate operation
                 new_gate_list = [gate_op]
                 for noise_op in noise_list:
                     noise_model = cirq.ConstantQubitNoiseModel(noise_op)
@@ -54,7 +32,13 @@ class Noisify:
         return result
 
     @staticmethod
-    def uniform_rotation_gate(sample_count: int) -> [of.QubitOperator]:
+    def introduce_noise_tree(op_tree: cirq.OP_TREE) -> cirq.OP_TREE:
+        noise_func = lambda: Noisify.uniform_rotation_gate(1000)
+        result = Noisify.introduce_noise_type(op_tree=op_tree, noise_func=noise_func)
+        return result
+
+    @staticmethod
+    def uniform_rotation_gate(sample_count: int) -> [cirq.Gate]:  # [of.QubitOperator]
         """
         Mimics the uniform fibonacci mapping on disk but then for a sphere.
         :param sample_count: Number of points to choose from on spherical surface (example: 1000)
