1a2fafab6c5024f7e8a563942d7fd5ddfb17d7d5
SeanvdMeer <MiniSean@users.noreply.github.com>
2020-09-14 18:25:49 +0200

    Added noise wrapper for wave function class

--- src/circuit_noise_extension.py
+++ src/circuit_noise_extension.py
@@ -38,6 +38,19 @@ class Noisify:
         return result
 
     @staticmethod
+    def introduce_noise(circuit: cirq.Circuit, noise_func: Callable[[], List[cirq.Gate]]) -> cirq.Circuit:
+        """
+        Introduces noise operations after each gate operation in a circuit.
+        Current noise operations are based on a random rotation to anywhere on the Bloch sphere.
+        :param noise_func: Callable function that takes not arguments and returns a list of cirq.Gates
+        :param circuit: Collection of gate operation to introduce noise to
+        :return: Same circuit with noise operations included
+        """
+        result = Noisify.introduce_noise_type(circuit.moments, noise_func)
+        # Create new circuit and set order strategy
+        return cirq.Circuit(result, strategy=cirq.InsertStrategy.EARLIEST)
+
+    @staticmethod
     def uniform_rotation_gate(sample_count: int) -> [cirq.Gate]:  # [of.QubitOperator]
         """
         Mimics the uniform fibonacci mapping on disk but then for a sphere.
@@ -61,3 +74,24 @@ class Noisify:
     def drift_gate(phi: float, theta: float) -> [of.QubitOperator]:
         """Translate polar and azimuthal angle rotation to a qubit operator"""
         return [cirq.rx(phi), cirq.rz(theta)]
+
+
+class NoiseWrapper(IWaveFunction):
+
+    # Implement abstract functions
+    def _generate_qubits(self) -> Sequence[cirq.Qid]:
+        return self._ideal_wave_function._generate_qubits()
+
+    def operations(self, qubits: Sequence[cirq.Qid]) -> cirq.OP_TREE:
+        yield Noisify.introduce_noise_type(self._ideal_wave_function.operations(qubits=qubits), self._noise_channel)
+
+    def initial_state(self, qubits: Sequence[cirq.Qid]) -> cirq.OP_TREE:
+        yield Noisify.introduce_noise_type(self._ideal_wave_function.initial_state(qubits=qubits), self._noise_channel)
+
+    def _generate_molecule(self, p: IParameter) -> of.MolecularData:
+        return self._ideal_wave_function._generate_molecule(p=p)
+
+    def __init__(self, w_class: IWaveFunction, noise_channel: List[cirq.Gate]):
+        self._ideal_wave_function = w_class
+        self._noise_channel = lambda: noise_channel  # Callable[[], List[cirq.Gate]]
+        IWaveFunction.__init__(self, w_class.operator_parameters, w_class.molecule_parameters)
--- src/main.py
+++ src/main.py
@@ -1,59 +1,64 @@
+import cirq  # TEMP
+
 from src.data_containers.model_hydrogen import HydrogenAnsatz, NoisyHydrogen
 from src.processors.processor_quantum import QPU
 from src.processors.processor_classic import CPU
 from src.calculate_minimisation import calculate_and_store
 from src.plot_minimisation import read_and_plot
-from src.circuit_noise_extension import Noisify
+from src.circuit_noise_extension import Noisify, NoiseWrapper
 
 
 if __name__ == '__main__':
 
-    print("H2-Molecule example. Finding expectation value for molecule with optimal atom distance (.7414 angstrom)")
-    print("Using a generated operator tree based on the UCCSD theorem")
-    uccsd_ansatz = HydrogenAnsatz()
-    parameters = uccsd_ansatz.operator_parameters
-
-    # Get resolved circuit
-    print('Show circuit with initial state preparation:')
-    circuit = QPU.get_initial_state_circuit(uccsd_ansatz)
-    circuit.append(uccsd_ansatz.circuit)
-    print(circuit)
-
-    # Get variational study
-    result = CPU.get_optimized_state(w=uccsd_ansatz, max_iter=1000)
-    print(f'Operator expectation value: {result.optimal_value}\nOperator parameters: {result.optimal_parameters}')
-
-    parameters.update(r=result)
-    resolved_circuit = QPU.get_resolved_circuit(circuit, parameters)
-    print(resolved_circuit)
-
-    # --------------------------
-
-    # # First test in applying noise to circuits or molecule classes directly
-    # print("\nTaking the existing circuit and apply a noise filter:")
-    # # Noisy circuit
-    # clean_hydrogen = HydrogenAnsatz()
-    # noise_circuit = QPU.get_initial_state_circuit(clean_hydrogen)  # Append state initialisation
-    # noise_circuit.append(clean_hydrogen.circuit)  # Append operations
-    # noise_circuit = Noisify.introduce_noise(noise_circuit)  # 'Noisify'
-    # print(noise_circuit)
+    # print("H2-Molecule example. Finding expectation value for molecule with optimal atom distance (.7414 angstrom)")
+    # print("Using a generated operator tree based on the UCCSD theorem")
+    # uccsd_ansatz = HydrogenAnsatz()
+    # parameters = uccsd_ansatz.operator_parameters
     #
-    # print("\nIntroduce noise directly into the initial state and operator preparations of the Hydrogen model:")
-    # # Noisy initial state
-    # noisy_hydrogen = NoisyHydrogen()
-    # noise_circuit = cirq.Circuit(noisy_hydrogen.initial_state(noisy_hydrogen.qubits))
-    # noise_circuit.append(noisy_hydrogen.operations(noisy_hydrogen.qubits))
-    # print(noise_circuit)
+    # # Get resolved circuit
+    # print('Show circuit with initial state preparation:')
+    # circuit = QPU.get_initial_state_circuit(uccsd_ansatz)
+    # circuit.append(uccsd_ansatz.circuit)
+    # print(circuit)
+
+    # # Get variational study
+    # result = CPU.get_optimized_state(w=uccsd_ansatz, max_iter=1000)
+    # print(f'Operator expectation value: {result.optimal_value}\nOperator parameters: {result.optimal_parameters}')
+    #
+    # parameters.update(r=result)
+    # resolved_circuit = QPU.get_resolved_circuit(circuit, parameters)
+    # print(resolved_circuit)
 
     # --------------------------
 
-    filename = 'H2_semi_minimisation'
-    # Run 'optimization and store data in a json format
-    wave_function_class = HydrogenAnsatz()
-    calculate_and_store(wave_class=wave_function_class, filename=filename)
-    # Read stored json format and express as a good old matlab plot
-    plt_obj = read_and_plot(filename=filename)
-    plt_obj.show()
+    # First test in applying noise to circuits or molecule classes directly
+    print("\nTaking the existing circuit and apply a noise filter:")
+    clean_hydrogen = HydrogenAnsatz()
+    clean_circuit = QPU.get_initial_state_circuit(clean_hydrogen)  # Append state initialisation
+    clean_circuit.append(clean_hydrogen.circuit)  # Append operations
+    # Test random noise models
+    # random_noise_channel = lambda: Noisify.uniform_rotation_gate(1000)
+    def phase_flip_channel(): return [cirq.phase_flip(p=.5)]
+    noise_circuit = Noisify.introduce_noise(clean_circuit, phase_flip_channel)  # 'Noisify'
+    print(noise_circuit)
+
+    # Test noise wrapper
+    print("\nIntroduce noise directly by applying noise wrapper with specified noise channel around the Hydrogen model:")
+    noise_channel = [cirq.bit_flip(p=.1), cirq.phase_flip(p=.1)]  # Example noise channel
+    noisy_hydrogen = NoiseWrapper(clean_hydrogen, noise_channel)
+    noise_circuit = QPU.get_initial_state_circuit(noisy_hydrogen)  # Append state initialisation
+    noise_circuit.append(noisy_hydrogen.circuit)  # Append operations
+    print(noise_circuit)
+
+    # --------------------------
+
+    # filename = 'H2_semi_minimisation'
+    # # Run 'optimization and store data in a json format
+    # wave_function_class = HydrogenAnsatz()
+    # calculate_and_store(wave_class=wave_function_class, filename=filename)
+    # # Read stored json format and express as a good old matlab plot
+    # plt_obj = read_and_plot(filename=filename)
+    # plt_obj.show()
 
     # --------------------------
 
