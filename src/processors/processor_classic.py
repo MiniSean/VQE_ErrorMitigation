b6376ae8fb27e3a480e612434da16155e8600409
SeanvdMeer <18538762+minisean@users.noreply.github.com>
2020-09-16 15:42:13 +0200

    Updates pre-calculated optimization with the ability to store standard deviation

--- src/processors/processor_classic.py
+++ src/processors/processor_classic.py
@@ -1,4 +1,6 @@
 import numpy as np
+import cirq
+from statistics import stdev, mean
 from scipy import optimize
 from openfermioncirq.optimization import OptimizationTrialResult
 from typing import List
@@ -35,17 +37,38 @@ class CPU:
         return optimize.minimize(fun=optimize_func, x0=initial_values, method=method, options=options)
 
     @staticmethod
-    def get_semi_optimized_ground_state(w: IWaveFunction, qpu_iter: int) -> List:
+    def get_semi_optimized_ground_state(w: IWaveFunction, cpu_iter: int, qpu_iter: int) -> List:
+        """
+        Uses pre-calculated data points to update molecule geometry.
+        Calculates optimized expectation value through either VariationalStudy (for noiseless wave functions)
+        or a custom optimization loop (for noisy wave functions).
+        The calculation of expectation values is repeated #cpu_iter number of times where each calculation has a max iteration of #qpu_iter.
+        The resulting information is stored in IContainer classes and returned as in a list.
+        :param w: (Noisy) IWaveFunction class that determines the calculation method for expectation value.
+        :param cpu_iter: Number of iterations for calculating the mean and std of expectation value results.
+        :param qpu_iter: Number of iterations for calculating the expectation value itself.
+        :return: List of IContainer classes.
+        """
         param_space = np.linspace(0.1, 3.0, 30)
-        print(param_space)
         molecule_params = w.molecule_parameters
         result = list()  # ITypedList(allowed_types=IContainer)
         for par in param_space:
             for i, key in enumerate(molecule_params):
                 molecule_params.dict[key] = round(par, 1)  # Temporary rounding to use correct mol_data
             w.update_molecule(molecule_params)
-            trial_result = CPU.get_optimized_state(w=w, max_iter=qpu_iter)  # Calculate trial result
-            container = IContainer(molecule_params, trial_result.optimal_value, w.molecule.fci_energy)
+
+            # Implement statistical average and standard deviation
+            opt_value_list = []
+            for j in range(cpu_iter):
+                if isinstance(w, INoiseWrapper):
+                    opt_value = CPU.get_custom_optimized_state(n_w=w, max_iter=qpu_iter)[0]
+                else:
+                    opt_value = CPU.get_optimized_state(w=w, max_iter=qpu_iter).optimal_value  # Calculate trial result
+                opt_value_list.append(opt_value)
+            avg_value = mean(opt_value_list)
+            std_value = stdev(opt_value_list)
+
+            container = IContainer(m_param=molecule_params, e_value=avg_value, std_value=std_value, fci_value=w.molecule.fci_energy)
             result.append(container)
         return result
 
