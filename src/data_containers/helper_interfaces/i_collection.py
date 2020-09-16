e8f88bb9784a46e9a517eda2ffc394963757a6c2
SeanvdMeer <18538762+minisean@users.noreply.github.com>
2020-09-16 15:32:20 +0200

    Updated to contain standard deviation value

--- src/data_containers/helper_interfaces/i_collection.py
+++ src/data_containers/helper_interfaces/i_collection.py
@@ -17,15 +17,20 @@ class IContainer:
     def set_measured_eigenvalue(self, e_value: float):
         self._measured_eigenvalue = e_value
 
+    def get_measured_standard_deviation(self) -> float:
+        return self._measured_standard_deviation
+
     def get_fci_value(self) -> float:
         return self._fci_value
 
     molecule_param = property(get_molecule_parameter)  # , set_molecule_parameter)
     measured_value = property(get_measured_eigenvalue)  # , set_measured_eigenvalue)
+    measured_std = property(get_measured_standard_deviation)  # , set_measured_eigenvalue)
     fci_value = property(get_fci_value)
 
-    def __init__(self, m_param: IParameter, e_value: float, fci_value: float):
+    def __init__(self, m_param: IParameter, e_value: float, std_value: float, fci_value: float):
         self._molecular_parameter = m_param['r0']
         print(self._molecular_parameter)
         self._measured_eigenvalue = e_value
+        self._measured_standard_deviation = std_value
         self._fci_value = fci_value
