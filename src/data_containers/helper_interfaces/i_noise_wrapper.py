794a224bd05b596334f71588bd15736aba28a060
SeanvdMeer <18538762+minisean@users.noreply.github.com>
2020-10-05 17:11:58 +0200

    Updated to identify difference between single and two qubit noise gates

--- src/data_containers/helper_interfaces/i_noise_wrapper.py
+++ src/data_containers/helper_interfaces/i_noise_wrapper.py
@@ -1,7 +1,7 @@
 import cirq
 import numpy as np
 import openfermion as of
-from typing import Sequence, List, Callable
+from typing import Sequence, List, Callable, Union
 
 from src.data_containers.helper_interfaces.i_parameter import IParameter
 from src.data_containers.helper_interfaces.i_wave_function import IWaveFunction
@@ -11,19 +11,36 @@ from src.circuit_noise_extension import Noisify
 
 class INoiseModel:
     def get_callable(self) -> Callable[[], List[cirq.Gate]]:
-        return lambda: self._noise_gates
+        return lambda: self._noise_gates_1q
 
     def get_description(self) -> str:
         return self._description
 
-    def __init__(self, noise_gates: List[cirq.Gate], description: str):
-        self._noise_gates = noise_gates
+    def __init__(self, noise_gates_1q: List[cirq.Gate], noise_gates_2q: List[cirq.Gate], description: str):
+        self._noise_gates_1q = noise_gates_1q
+        self._noise_gates_2q = noise_gates_2q
         self._description = description
+
+    def magic_check(self):
+        for gate in self._noise_gates_1q:
+            if gate.num_qubits != 1:
+                raise ValueError('Should be single qubit noise gate')
+        for gate in self._noise_gates_2q:
+            if gate.num_qubits != 2:
+                raise ValueError('Should be two qubit noise gate')
+
+    def get_operators(self, *qubits: cirq.Qid) -> List[cirq.Operation]:
+        if len(qubits) == 1:
+            return [gate.on(*qubits) for gate in self._noise_gates_1q]
+        elif len(qubits) == 2:
+            return [gate.on(*qubits) for gate in self._noise_gates_2q]
+        else:
+            raise NotImplemented('Can only construct single and two qubit noise operators')
 
     def get_effective_gate(self, gate: np.ndarray) -> np.ndarray:
         if gate.shape[0] == gate.shape[1] == 2:  # Pauli Transfer Matrix (single qubit)
             # Simple single qubit case
-            for noise_gate in self._noise_gates:
+            for noise_gate in self._noise_gates_1q:
                 effective_gate = np.zeros(gate.shape, dtype=complex)
                 try:  # Try mixture
                     mixture = cirq.mixture(noise_gate)
@@ -34,15 +51,12 @@ class INoiseModel:
                     pass
         elif gate.shape[0] == gate.shape[1] == 4:  # Pauli Transfer Matrix (double qubits)
             # Two qubit case
-            for noise_gate in self._noise_gates:
+            for noise_gate in self._noise_gates_2q:
                 effective_gate = np.zeros(gate.shape, dtype=complex)
-                try:
+                try:  # Try mixture
                     mixture = cirq.mixture(noise_gate)
-                    for (p_A, basis_A) in mixture:
-                        for (p_B, basis_B) in mixture:
-                            basis_prod = np.kron(basis_A, basis_B)
-                            p_prod = p_A * p_B
-                            effective_gate += p_prod * (basis_prod @ (gate @ basis_prod.conj().transpose()))
+                    for (p, basis) in mixture:
+                        effective_gate += p * (basis @ (gate @ basis.conj().transpose()))
                     gate = effective_gate
                 except TypeError:
                     pass
