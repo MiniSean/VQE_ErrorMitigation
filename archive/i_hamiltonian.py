ee5174fb9c3a43d3edcf7cb0422ccb727faadf04
SeanvdMeer <18538762+minisean@users.noreply.github.com>
2020-10-15 14:11:29 +0200

    Removed empty file

--- archive/i_hamiltonian.py
+++
@@ -1,9 +0,0 @@
-import cirq
-import openfermioncirq
-from openfermion import jordan_wigner, QubitOperator
-
-from src.data_containers.helper_interfaces.i_wave_function import IWaveFunction
-
-
-class IHamiltonian:
-    pass
