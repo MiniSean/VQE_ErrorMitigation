1d00978edf18453746a569b7fc2e7aa510c5c47d
SeanvdMeer <18538762+minisean@users.noreply.github.com>
2020-10-15 16:24:30 +0200

    Removed openfermioncirq dependency

--- src/data_containers/helper_interfaces/i_parameter.py
+++ src/data_containers/helper_interfaces/i_parameter.py
@@ -1,6 +1,6 @@
 from typing import (Dict)
 import cirq
-from openfermioncirq.optimization import OptimizationTrialResult
+# from openfermioncirq.optimization import OptimizationTrialResult
 
 
 class IParameter:
@@ -16,11 +16,17 @@ class IParameter:
     def __init__(self, parameters: Dict[str, float]):
         self._p = parameters
 
-    def update(self, r: OptimizationTrialResult):
-        if len(r.optimal_parameters) != len(self.dict):
+    # def update(self, r: OptimizationTrialResult):
+    #     if len(r.optimal_parameters) != len(self.dict):
+    #         raise IndexError("Dimensions of optimized trial result does not correspond to IParameter")
+    #     for i, key in enumerate(self.dict.keys()):
+    #         self[key] = r.optimal_parameters[i]
+
+    def update(self, v: [float]):
+        if len(v) != len(self.dict):
             raise IndexError("Dimensions of optimized trial result does not correspond to IParameter")
-        for i, key in enumerate(self.dict.keys()):
-            self[key] = r.optimal_parameters[i]
+        for i, key in enumerate(self.dict.keys()):  # Dirty set parameter values
+            self[key] = v[i]
 
     def __iter__(self):
         return self.dict.__iter__()
